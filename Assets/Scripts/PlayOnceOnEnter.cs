e5134da2eba488b603c40cc1bd962273e3af0600
Charley <45150606+W0lfguts@users.noreply.github.com>
2020-12-03 09:06:00 +0000

    Post Processing Additions

---
+++ Assets/Scripts/PlayOnceOnEnter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnceOnEnter : MonoBehaviour {
+
+	public AudioClip enterSound;
+
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{	       
+		Debug.Log("Player Object Entered Target Area"); 
+		audio.PlayOneShot(enterSound, 0.7f);
+	}
+
+	void OnTriggerExit(Collider coll)
+	{
+		Debug.Log("Player Object Exited Target Area");
+	}
+
+}
